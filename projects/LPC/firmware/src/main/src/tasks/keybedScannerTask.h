a6a6fe63df4e6b27a4235ab3870a3066341388b8
Klaus Strohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2022-09-20 18:27:42 +0200

    Update keybedScannerTask.h
    fixed non-working m_keybedEventLED.timedOn() trigger

--- projects/LPC/firmware/src/main/src/tasks/keybedScannerTask.h
+++ projects/LPC/firmware/src/main/src/tasks/keybedScannerTask.h
@@ -15,7 +15,7 @@ namespace Task
     using Task::Task;
 
    private:
-    unsigned        tan;
+    unsigned        m_tan;
     IOpins::IOpin&  m_keybedEventLED;
     IOpins::IOpin&  m_dataLossLED;
     tUsbMidiWriter& m_keyEventWriter;
@@ -34,22 +34,24 @@ namespace Task
     // scanner is run unconditionally
     inline void run(void)
     {
-      uint32_t event = 0;
+      uint32_t event;
+      int      hadEvent = 0;
       while ((event = IPC_M4_KeyBuffer_ReadBuffer()))  // reads the latest key up/down events from M0 ring buffer
       {
+        hadEvent = 1;
         // tan
-        tan = (tan + 1u) & 0b11111111111111;
+        m_tan = (m_tan + 1u) & 0b11111111111111;
 
         if (m_keyEventWriter.claimBufferElements(3))  // 3 4-byte frames available ?
         {
-          m_keyEventWriter.write(KEYBED_DATA_CABLE_NUMBER, 0xF0, Usb::getSysexHiByte(tan), Usb::getSysexLoByte(tan));                              // start, tanH/tanL
+          m_keyEventWriter.write(KEYBED_DATA_CABLE_NUMBER, 0xF0, Usb::getSysexHiByte(m_tan), Usb::getSysexLoByte(m_tan));                          // start, tanH/tanL
           m_keyEventWriter.write(KEYBED_DATA_CABLE_NUMBER, Usb::getSysexLoByte(event), Usb::getSysexHi4Byte(event), Usb::getSysexHi3Byte(event));  // keynum+make, timeHH/HL
           m_keyEventWriter.writeLast(KEYBED_DATA_CABLE_NUMBER, Usb::getSysexHi2Byte(event), Usb::getSysexHiByte(event), 0xF7);                     // timeLH/LL, end
         }
         else  // Data Loss !!!
           m_dataLossLED.timedOn(1);
       }
-      if (event)
+      if (hadEvent)
         m_keybedEventLED.timedOn(2);
     };
   };
