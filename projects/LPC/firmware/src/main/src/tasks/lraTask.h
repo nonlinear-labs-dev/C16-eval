56c6681a9ea568ceb9e428e1de4786ea2e94009b
Klaus Strohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2022-10-17 13:29:03 +0200

    lraTask.h : simplification (use pin address directly)

--- projects/LPC/firmware/src/main/src/tasks/lraTask.h
+++ projects/LPC/firmware/src/main/src/tasks/lraTask.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "io/pin_setup.h"
 #include "drv/IoPin.h"
 #include "tasks/mtask.h"
 
@@ -47,15 +48,15 @@ namespace Task
     class LRA
     {
      private:
-      IOpins::IOpin& m_lraPin;
-      unsigned       m_slice { 0 };
-      uint64_t       m_pattern { 0 };
-      uint64_t       m_patternReload { 0 };
-      unsigned       buttons { 0 };
+      pIO32    m_pLraPin;
+      unsigned m_slice { 0 };
+      uint64_t m_pattern { 0 };
+      uint64_t m_patternReload { 0 };
+      unsigned buttons { 0 };
 
      public:
-      constexpr LRA(IOpins::IOpin& lraPin)
-          : m_lraPin(lraPin) {};
+      constexpr LRA(pIO32 pLraPin)
+          : m_pLraPin(pLraPin) {};
 
       inline void startPattern(uint64_t const pattern)
       {
@@ -66,8 +67,15 @@ namespace Task
         else
           m_patternReload = m_pattern;
         pinLRA_nENABLE = !0;
-        m_lraPin.set(0);
+        (*m_pLraPin)   = 0;
       };
+
+      inline void reset(void)
+      {
+        (*m_pLraPin) = 0;
+        m_slice      = 0;
+        m_pattern = m_patternReload = 0;
+      }
 
       int process(void)
       {
@@ -83,8 +91,8 @@ namespace Task
           m_slice--;
           if (m_slice)
           {
-            activity = 1;
-            m_lraPin.set(0 != (m_pattern & (uint64_t(1) << 63)));
+            activity     = 1;
+            (*m_pLraPin) = (0 != (m_pattern & (uint64_t(1) << 63)));
             m_pattern <<= 1;
           }
         }
@@ -98,9 +106,22 @@ namespace Task
   {
    private:
     IOpins::IOpin& m_lraActivityLED;
-    IOpins::IOpin  m_lraPin[8] { pinLRA_0, pinLRA_1, pinLRA_2, pinLRA_3, pinLRA_4, pinLRA_5, pinLRA_6, pinLRA_7 };
-    LRA::LRA       m_lra[8] { m_lraPin[0], m_lraPin[1], m_lraPin[2], m_lraPin[3], m_lraPin[4], m_lraPin[5], m_lraPin[6], m_lraPin[7] };
+    LRA::LRA       m_lra[8] { &pinLRA_0, &pinLRA_1, &pinLRA_2, &pinLRA_3, &pinLRA_4, &pinLRA_5, &pinLRA_6, &pinLRA_7 };
     unsigned       buttons { 0 };
+    unsigned       lraNum { 0 };
+
+    void stopAll(void)
+    {
+      pinLRA_nENABLE = !0;
+      m_lra[0].reset();
+      m_lra[1].reset();
+      m_lra[2].reset();
+      m_lra[3].reset();
+      m_lra[4].reset();
+      m_lra[5].reset();
+      m_lra[6].reset();
+      m_lra[7].reset();
+    }
 
    public:
     constexpr LRAHandler(uint32_t const delay, uint32_t const period, IOpins::IOpin& lraActivityLED)
@@ -109,24 +130,34 @@ namespace Task
 
     inline void body(void)
     {
+
+      // start of test code
       unsigned newButtons = IOpins::readButtons();
       if (buttons == 0 && newButtons == 0b0001)
-        m_lra[0].startPattern(LRA::blip_l4_s4);
+        m_lra[lraNum].startPattern(LRA::blip_l4_s4);
       else if (buttons == 0 && newButtons == 0b0010)
-        m_lra[0].startPattern(LRA::cont_s1);
+        m_lra[lraNum].startPattern(LRA::cont_s1);
       else if (buttons == 0 && newButtons == 0b0100)
-        m_lra[0].startPattern(LRA::off);
-      else if (newButtons == 0b1000)
-        ehcSetup_Unloaded(0);
-      else
-        ehcSetup_Default(0);
+        stopAll();
+      else if (buttons == 0 && newButtons == 0b1000)
+      {
+        stopAll();
+        lraNum = (lraNum + 1) % 6;
+      }
+      else if (buttons == 0b1010 && newButtons == 0b1010)
+      {
+        while (1)  // catastrophic errors test
+          *((volatile uint32_t*) buttons++) = 0;
+      }
 
       buttons = newButtons;
+      // end of test code
 
       int activity   = (m_lra[0].process() + m_lra[1].process() + m_lra[2].process() + m_lra[3].process()
                       + m_lra[4].process() + m_lra[5].process() + m_lra[6].process() + m_lra[7].process());
-      pinLRA_nENABLE = activity != 0;
-      m_lraActivityLED.set(activity);
+      pinLRA_nENABLE = !activity;
+      if (activity)
+        m_lraActivityLED.timedOn(1);
     }
   };
 
