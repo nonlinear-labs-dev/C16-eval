0d280fa1c304c3a9aadcc0c853d0c8250fc78dda
Klaus Strohhaecker <klaus.strohhaecker@nonlinear-labs.de>
2022-11-01 11:27:04 +0100

    clang-format
    remove LPC43xx.h from processing as this file is large and takes many seconds to scan

--- projects/LPC/clang-format_all.bat
+++ projects/LPC/clang-format_all.bat
@@ -1,4 +1,8 @@
 %~d0
 cd %~dp0
-for /R %%f in (*.c *.cpp *.h *.inc) do clang-format.exe --verbose -i -style=file "%%f"
+for /R %%G in (*.c *.cpp *.h *.inc) do (
+  if "%%~nG" NEQ "LPC43xx" (
+    clang-format.exe --verbose -i -style=file "%%G"
+  )
+)
 pause
